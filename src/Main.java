9ee6b63958f87dcc7541d21c3206ebd9436ff718
Alissa-1 <alena.cheremukhina@gmail.com>
2022-05-20 18:11:38 +0300

    Code format

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int price = 20005;
         int rubPerMile = 20;
         int miles = price / rubPerMile;
-        if (price>0) {
+        if (price > 0) {
             System.out.println("Вам начислено " + miles + " миль");
         } else {
             System.out.println("Неверно указана цена билета");
